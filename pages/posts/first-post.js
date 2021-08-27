e9628e66ff3e34dcf09b8f6ac78a4c706db43dea
Miika Härmä <harmamiika@gmail.com>
2021-08-27 23:28:13 +0300

    completed navigation, started part 3

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
