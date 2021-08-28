accc5abb3f05fbc39c3c4aed1adbb371315a5b12
Miika Härmä <harmamiika@gmail.com>
2021-08-28 13:40:54 +0300

    compe´leted part 3  assets - metadata and css

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+// top level component used in all differnet pages
+// add global css here
