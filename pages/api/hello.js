c102818da06a3da583a71f830d50e6a116b02549
Miika Härmä <harmamiika@gmail.com>
2021-08-29 12:24:50 +0300

    completed part 6 - api routes

---
+++ pages/api/hello.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.status(200).json({ text: 'Hello' });
+}
+
+// do not fetch from getStaticProps of getStaticPaths
+
+// just write dbsaving code here for example
